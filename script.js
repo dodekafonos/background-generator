840b896a93e98a84d829ae84d859909cca1b24dc
jonas <jonasalvesbueno@gmail.com>
2023-03-25 12:41:32 -0300

    Refactoring

--- script.js
+++ script.js
@@ -6,18 +6,20 @@ let hex1 = document.querySelector("#cor1-txt");
 let hex2 = document.querySelector("#cor2-txt");
 let cheat = document.querySelector("#cheat");
 
-hex1.innerHTML += color1.value
-hex2.innerHTML += color2.value
-
-cheat.innerHTML = "body { background: linear-gradient(to right, " + color1.value + ", " + color2.value + ") }"
 
 function inputValue() {
-    console.log(color1.value)
     fundo.style.background = "linear-gradient(to right," + color1.value + ", " + color2.value + ")"
     hex1.innerHTML = "Primeira cor: " + color1.value
     hex2.innerHTML = "Segunda cor: " + color2.value
+    cheat.innerHTML = "body { background: linear-gradient(45deg, " + color1.value + ", " + color2.value + ") }"
 }
 
 color1.addEventListener("input", inputValue)
 
 color2.addEventListener("input", inputValue)
+
+
+hex1.innerHTML += color1.value
+hex2.innerHTML += color2.value
+
+cheat.innerHTML = "body { background: linear-gradient(45deg, " + color1.value + ", " + color2.value + ") }"
